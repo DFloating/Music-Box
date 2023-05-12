d52f05f135dc0ae1573bf8a87552450f1ea2605e
JoshuaLing <joshualing81@gmail.com>
2023-05-12 12:20:46 +1000

    api login

--- src/component/MusicBox.jsx
+++ src/component/MusicBox.jsx
@@ -4,27 +4,20 @@ import {Buffer} from "buffer";
 const client_id = 'c9cf6d49f751465e83d620840d69e274';
 const client_secret = '773eac77730f4e158c724ca72918b30c';
 
-const authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))},
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
 function MusicBox() {
-  axios.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      let token = body.access_token;
-    }
-  });
 
-MusicBox();
+  const CLIENT_ID = 'c9cf6d49f751465e83d620840d69e274';
+const CLIENT_SECRET = '773eac77730f4e158c724ca72918b30c';
+const REDIRECT_URI = 'http://localhost:5173';
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+const RESPONSE_TYPE = 'token';
+
+
+
   return (
     <div>
       Our MusicBox
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
     </div>
   );
 };
