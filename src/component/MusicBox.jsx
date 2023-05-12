d6772c8107559c320edb5c04b413d13f80f05c5c
YX <asdf@hotmail.com>
2023-05-12 11:18:23 +0800

    dont use this-Eugene

--- src/component/MusicBox.jsx
+++ src/component/MusicBox.jsx
@@ -1,32 +1,43 @@
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {Buffer} from "buffer";
 
 const client_id = 'c9cf6d49f751465e83d620840d69e274';
 const client_secret = '773eac77730f4e158c724ca72918b30c';
 
-const authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))},
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
 function MusicBox() {
-  axios.post(authOptions, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      let token = body.access_token;
-    }
-  });
+  const [token, setToken] = useState('');
 
-MusicBox();
+  useEffect(() => {
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      method: 'post',
+      data: 'grant_type=client_credentials',
+      // params: {
+      //   grant_type: 'client_credentials',
+      // },
+      headers: {
+        'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+
+    axios(authOptions)
+      .then(response => {
+        const token = response.data.access_token;
+        setToken(token);
+      })
+      .catch(error => {
+        // Handle error
+      });
+  }, []);
+
   return (
     <div>
-      Our MusicBox
+      <p>Access Token: {token}</p>
+      <p>Our MusicBox</p>
     </div>
   );
-};
+}
 
 export default MusicBox;
