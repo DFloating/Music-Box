cd670a094d8e3841a30e2535e99fb7a6f81bc8bb
Jordy <jordy@Jordys-MacBook-Pro.local>
2023-05-18 15:14:45 +1000

    MusicList and MusicShowAll

--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -1,50 +1,52 @@
-import React from 'react';
-// import './App.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import MusicPlayer from './MusicPlayer';
 
+const supabase = createClient(
+  "https://mlkeyxaswemirdbuvayj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sa2V5eGFzd2VtaXJkYnV2YXlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NDEzMjIyMiwiZXhwIjoxOTk5NzA4MjIyfQ.JcfoyFRW8NWBEnmVtyj5icn4exRS0fuUWeJKzKqZhDA"
+);
+
 const MusicList = () => {
-    // Define state variables
-    const [searchTerm, setSearchTerm] = useState('');
-    const [musicList, setMusicList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [musicList, setMusicList] = useState([]);
 
-    // mock data for list of songs
-const songsList = [
-    { artist: "Adele", title: "Someone Like You", genre: "Pop" },
-    { artist: "Eminem", title: "Lose Yourself", genre: "Hip-Hop" },
-    { artist: "Queen", title: "Bohemian Rhapsody", genre: "Rock" },
-    { artist: "Beyonce", title: "Crazy in Love", genre: "R&B" },
-    { artist: "Ed Sheeran", title: "Thinking Out Loud", genre: "Pop" },
-    { artist: "AC/DC", title: "Highway to Hell", genre: "Rock" }
-    ];
-
-    // Function to handle input change
-const handleChange = (e) => {
-      setSearchTerm(e.target.value);
-    };
-  
-    // Function to handle search
-const handleSearch = () => {
-      // Call an API or fetch data from the server
-      // Filter the data based on the search term
-      // Set the filtered music list to the state
-    };
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
     
-    return (
-      <div>
-        <input type="text" value={searchTerm} onChange={handleChange} />
-        <button onClick={handleSearch}>Find Track</button>
-        {musicList.map((music) => (
-          <div key={music.id}>
-            <h3>{music.title}</h3>
-            <p>{music.artist}</p>
-            <p>{music.genre}</p>
-          </div>
-        ))}
-       <MusicPlayer /> 
-      </div>
-    );
   };
+
+  useEffect(() => {
+    const handleSearch = async () => {
+        const { data, error } = await supabase
+          .from('songs')
+          .select()
+          .eq('title', `${searchTerm}`)
+          
   
+        console.log(data);
+        setMusicList(data);
+      
+    };
+
+    handleSearch();
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <input type="text" value={searchTerm} onChange={handleChange} />
+      <button onClick={() => {}}>Find Track</button>
+      {musicList.map((music) => (
+        <div key={music.id}>
+          <h3>{music.title}</h3>
+          <p>{music.artist}</p>
+          <p>{music.genre}</p>
+        </div>
+      ))}
+      {/* <MusicPlayer /> */}
+    </div>
+  );
+};
+
 export default MusicList;
