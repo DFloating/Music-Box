a95b8a4c1400aa303bd80279acedbaf927f100a7
Jordy <jordy@Jordys-MacBook-Pro.local>
2023-05-19 10:08:34 +1000

    Text yo

--- src/components/MusicBox.jsx
+++ src/components/MusicBox.jsx
@@ -5,13 +5,23 @@ function MusicBox(){
   return (
     <div className="Intro">
       <h1>Music-Box</h1>
-    <h3>featuring JordyJord, Genie-theCoolKat, Josh the unnamed & Dfloaty</h3>
+    <h3>Music-Box offers a user-friendly interface, allowing young artists to showcase their talents and gain exposure to a wider audience.</h3>
     
-    <p1># How to Use this Music Box? #</p1>
+    <p1> The company’s mission is to support emerging talent in the music industry, and provide a platform for music lovers to discover new and exciting artists. </p1>
     
-    <p>Upload your songs on the Music Upload tabs!
-    View all your uploaded songs in the Music List.
-    Play, Pause and Stop songs directly from the list!</p>
+    <p>By partnering with record labels and established artists, Music-Box also offers exclusive content, such as interviews, live streams, and behind-the-scenes access to the music industry.
+      Music-Box’s database is curated by a team of music experts, ensuring high quality and diverse content. Genres ranging from hip-hop, pop, EDM, rock, and more are all represented on the platform.
+      Music-Box aims to be the go-to estination for the youth, music enthusiasts, and up-and-coming artists seeking a platform to share their music. With its engaging interface, diverse content, and commitment to supporting new talent, Music-Box is poised to become a game-changer in the music industry.</p>
+      
+      <p className="Jordy">HOW TO USE:</p>
+      <p className="Jordy">1. Browse the website's database of music tracks.</p>
+      <p className="Jordy">2. Search for specific songs or albums using the search bar.</p>
+      <p className="Jordy">3. Click on a track to listen to a preview of the song.</p>
+      <p className="Jordy">4. If you like the track, click on the "Add to Playlist" button to create your own personal playlist.</p>
+      <p className="Jordy">5. If the track is not available on the website's database, you can upload it yourself by clicking on the "Upload" button.</p>
+      <p className="Jordy">6. Fill out the necessary information in the upload page, including the song title, artist name, and music file.</p>
+      <p className="Jordy">7. Once your upload is complete, the song will be added to the website's database for other users to discover and enjoy.</p>
+      <p className="Jordy">8. Share your favorite tracks and playlists with your friends and fellow music lovers on social media platforms.</p>
    
    
    </div>
